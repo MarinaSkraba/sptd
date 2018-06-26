d16663785700f165278444d4f2ca71856e89a3f9
MarinaSkraba <40609331+MarinaSkraba@users.noreply.github.com>
2018-06-26 16:18:03 -0300

    Add files via upload

---
+++ Dao.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpr.irati.dao;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.criteria.Order;
+
+public interface Dao<T> {
+
+    public T buscarPorId(Serializable id);
+
+    public void salvar(T t);
+
+    public void alterar(T t);
+    
+    public void excluir(T t);
+    
+    public List<T> buscarTodos(Class<T> clazz);
+    
+    
+}
---
+++ GenericDAO.java
@@ -0,0 +1,76 @@
+package br.edu.ifpr.irati.dao;
+
+
+;
+import br.edu.ifpr.irati.util.HibernateUtil;
+import java.io.Serializable;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+
+public class GenericDAO<T> implements Dao<T> {
+
+    private final Class classePersistente;
+
+    public GenericDAO(Class classePersistente) {
+        this.classePersistente = classePersistente;
+    }
+
+    @Override
+    public T buscarPorId(Serializable id) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        T t = (T) session.load(classePersistente, id);
+        session.clear();
+        session.close();
+        return t;
+    }
+      @Override
+    public void salvar(T t) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+
+        session.save(t);
+
+        session.getTransaction().commit();
+        session.clear();
+        session.close();
+    }
+
+    @Override
+    public void alterar(T t) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+
+        session.update(t);
+
+        session.getTransaction().commit();
+        session.clear();
+        session.close();
+    }
+
+    @Override
+    public void excluir(T t) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+
+        session.delete(t);
+
+        session.getTransaction().commit();
+        session.clear();
+        session.close();
+    }
+    
+    @Override
+    public List<T> buscarTodos(Class<T> clazz) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        String hql = "from "+clazz.getCanonicalName();
+        Query query = session.createQuery(hql);
+        List results = query.list();
+        session.clear();
+        session.close();
+        return results;
+    }
+
+            
+}
