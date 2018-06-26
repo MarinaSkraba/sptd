cf9f53c184134187fc5e3b1e77699d93ddf530be
MarianaCequinel <40609293+MarianaCequinel@users.noreply.github.com>
2018-06-26 16:13:32 -0300

    Update ProjetoExtensao.java

--- ProjetoExtensao.java
+++ ProjetoExtensao.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+estou meio triste confesso que queria que fosse ao vivasso 
 package br.edu.ifpr.irati.modelo;
 
 import java.io.Serializable;
