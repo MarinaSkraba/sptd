a1d4b97da902ffb59c10266c10a6e746c6fb3a60
williammagnabosco <40609759+williammagnabosco@users.noreply.github.com>
2018-06-28 09:07:00 -0300

    Add files via upload

---
+++ LoginMB.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpr.irati.mb;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author willi
+ */
+
+@ManagedBean
+@SessionScoped
+public class LoginMB {
+    
+    private String usuario;
+    private String senha;
+
+    /**
+     * @return the usuario
+     */
+    public String getUsuario() {
+        return usuario;
+    }
+
+    /**
+     * @param usuario the usuario to set
+     */
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+    
+}
