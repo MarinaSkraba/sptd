7bcaed24d73370bd51a595a99f8a882d3fe28677
MarinaSkraba <40609331+MarinaSkraba@users.noreply.github.com>
2018-07-02 13:52:14 -0300

    Update LoginMB.java

--- LoginMB.java
+++ LoginMB.java
@@ -5,61 +5,54 @@
  */
 package br.edu.ifpr.irati.mb;
 
+import br.edu.ifpr.irati.dao.Dao;
+import br.edu.ifpr.irati.dao.GenericDAO;
+import br.edu.ifpr.irati.modelo.DiretorEnsino;
+import br.edu.ifpr.irati.modelo.Docente;
+import br.edu.ifpr.irati.modelo.Usuario;
+import br.edu.ifpr.irati.util.Digest;
+import br.edu.ifpr.irati.util.HashGenerationException;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-
-/**
- *
- * @author willi
- */
 
 @ManagedBean
 @SessionScoped
 public class LoginMB {
-    
-    private String usuario;
-    private String senha;
+
+    private Usuario usuario;
 
     public LoginMB() {
-        usuario = "";
-        senha = "";
-        
+        usuario = new Usuario();
     }
 
-    public boolean verificarLogin(String email, String senha) throws HashGenerationException{
-        
+    public void verificarLogin() throws HashGenerationException {
+        String senhaSHA512 = Digest.hashString(usuario.getSenhaAlfanumerica(), "SHA-512");     
+        System.out.println("Chegou criptografia");
+        System.out.println(usuario.getEmail());
+        System.out.println(senhaSHA512);
+        Dao<Usuario> usuarioDao = new GenericDAO<>(Usuario.class);
+        usuario = usuarioDao.verificarUsuario(usuario.getEmail(), senhaSHA512);
+        System.out.println("Chegou object");
+        System.out.println(usuario);
+
+        efetuarLogin(usuario);
     }
-    public void efetuarLogin(){
-        
+
+    public String efetuarLogin(Object o) {
+        if (o instanceof Docente) {
+            return "/PTD";
+        } else if (o instanceof DiretorEnsino) {
+            return "/index";
+        }
+        System.out.println("Usuário não existe");
+        return null;
     }
-    /**
-     * @return the usuario
-     */
-    public String getUsuario() {
+
+    public Usuario getUsuario() {
         return usuario;
     }
 
-    /**
-     * @param usuario the usuario to set
-     */
-    public void setUsuario(String usuario) {
+    public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
     }
-
-    /**
-     * @return the senha
-     */
-    public String getSenha() {
-        return senha;
-    }
-
-    /**
-     * @param senha the senha to set
-     */
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-    
-    
-    
 }
